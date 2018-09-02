9df2c970e2b8caaa4e75347ce31e1ba1a095cb27
misraarvind100 <misraarvind100@yahoo.com>
2018-09-02 17:48:52 +0530

    First Commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+
+// Set first img opacity
+
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e){
+
+    //Reset the opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+
+    // change current image to src of clicked image
+    current.src = e.target.src;
+
+    //Add fade-in class
+    current.classList.add('fade-in');
+
+    //Remove fade-in class after 0.5s
+
+    setTimeout(()=> current.classList.remove('fade-in'),500);
+
+    //change the opacity to opacity var
+    e.target.style.opacity = opacity;
+}
